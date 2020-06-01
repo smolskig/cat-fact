7c7dd3097b4504661259278371feb96c9108cfed
smolskig <1gabego@gmail.com>
2020-06-01 18:54:13 -0300

    bkp

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <span >Click on the card to get a new random photo and fact</span>
     </div>
     <FactCard msg="Welcome to Your Vue.js App"/>
-    <div id="adsgoeshere" style="background: #1d1f29; padding-top:60px; text-align: center;" v-html="adsenseContent"></div>
+    <div id="adsgoeshere" style="position:absolute; bottom:0; background: #1d1f29; padding-top:60px; text-align: center;" v-html="adsenseContent"></div>
   </div>
 </template>
 
